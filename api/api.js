5e8ad1eed7bb96778afcbe92195de496334c7f72
huajie1988 <whuajie1988@gmail.com>
2016-05-04 17:39:18 +0800

    添加工具类和list接口简单实现

--- api/api.js
+++ api/api.js
@@ -1,9 +1,104 @@
 var express = require('express');
 var router = express.Router();
+var dao = require('../dao/indexDAO.js');
+var util = require('../util');
+
 
 /* GET API */
 router.get('/', function(req, res, next) {
   res.send('hello world!');
 });
+router.get('/list', function(req, res, next) {
+	var page=req.param('page') || 1;
+	var tag=req.param('tag');
+	var keyword=req.param('keyword');
+	var author=req.param('author');
+	var auth=util.Cookies.getCookie(req,'auth');
 
+	var list=[{
+				"articles": [{
+			            "id": 1001,
+			            "title": "文章标题1",
+			            "description": "文章描述1",
+			            "author": {
+			                "id": "xykbear",
+			                "name": "XYKbear"
+			            },
+			            "timestamp": 1459346686265,
+			            "thumb": "images/thumb.jpg",
+			            "tags": [
+			                "JavaScript",
+			                "code"
+			            ],
+			            "actions": [
+			                "edit",
+			                "delete"
+			            ]
+			        },
+			        {
+			            "id": 1002,
+			            "title": "文章标题2",
+			            "description": "文章描述2",
+			            "author": {
+			                "id": "xykbear",
+			                "name": "XYKbear"
+			            },
+			            "timestamp": 1459346686265,
+			            "thumb": "images/thumb.jpg",
+			            "tags": [
+			                 "ACG",
+			                 "菟饼"
+			            ],
+			            "actions": []
+			        }
+			    ],
+			    "pagenation": {
+			        "current": 1,
+			        "max": 2
+			    }
+			},
+			{
+				"articles": [{
+		            "id": 1003,
+		            "title": "文章标题3",
+		            "description": "文章描述3",
+		            "author": {
+		                "id": "xykbear",
+		                "name": "XYKbear"
+		            },
+		            "timestamp": 1459346686265,
+		            "thumb": "images/thumb.jpg",
+		            "tags": [
+		                "code",
+		                "PHP"
+		            ],
+		            "actions": [
+		                "edit"
+		            ]
+		        },
+		        {
+		            "id": 1004,
+		            "title": "文章标题4",
+		            "description": "文章描述4",
+		            "author": {
+		                "id": "xykbear",
+		                "name": "XYKbear"
+		            },
+		            "timestamp": 1459346686265,
+		            "thumb": "images/thumb.jpg",
+		            "tags": [
+		                "ACG",
+		                "Steam"
+		            ],
+		            "actions": []
+		        }],
+			    "pagenation": {
+			        "current": 1,
+			        "max": 2
+			    }
+			}]
+
+	page=Math.abs(page-1)
+	res.send(list[page]||{});
+});
 module.exports = router;
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 // var routes = require('./routes/index');
-// var users = require('./routes/users');
+var api = require('./api/api');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'app')));
 
 // app.use('/', routes);
-// app.use('/users', users);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
